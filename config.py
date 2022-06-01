d5c6ccae9269604e13e629ed0b4192d7a54c633f
Johnny Tsou <aclich.32@gmail.com>
2022-06-01 19:40:53 +0800

    Add config 移除http request中伺服器與登入資訊，使用config.py取代

---
+++ config.py
@@ -0,0 +1,7 @@
+
+class Config(object):
+    def __init__(self) -> None:
+        self.server_url = "http://localhost:5000"
+        self.sa_email = "example@email.com"
+        self.sa_pwd = "example_pwd"
+    
