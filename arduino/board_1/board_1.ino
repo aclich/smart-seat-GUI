a1e3af4e0a7515e478f7458a93cd3395430cf077
Vincent <a39746017@gmail.com>
2022-06-17 15:03:38 +0800

    重構arduino程式碼

--- arduino/board_1/board_1.ino
+++ arduino/board_1/board_1.ino
@@ -1,252 +1,71 @@
 #include <stdlib.h>
-#define fsr_pin A0
-#define fsr_pin2 A1
-#define fsr_pin3 A2
-#define fsr_pin4 A3
-#define fsr_pin5 A4
-#define fsr_pin6 A5
-#define fsr_pin7 A6
-#define fsr_pin8 A7
-#define fsr_pin9 A8
-#define fsr_pin10 A9
-#define fsr_pin11 A10
-#define fsr_pin12 A11
-#define fsr_pin13 A12
-#define fsr_pin14 A13
-#define fsr_pin15 A14
-#define fsr_pin16 A15
-//int fsr_pin3 = 8;
-char serial;
 
-void setup() 
-{ 
-Serial.begin(9600); // 9600 bps
-} 
-void loop() 
+#define PressureThres 200
+#define SensorCount 16
+#define BoardName "B1"
+
+byte pins[SensorCount] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
+String sensor_id[SensorCount] = {"0", "1", "2", "3", "5", "6", "7", "8", "10", "11", "12", "13", "15", "16", "17", "18"};
+int mode = 1;
+
+int get_value(byte pin){
+  int value = analogRead(pin);
+  return value > PressureThres ? value : 0;
+  }
+
+void mode_1(){
+  for (int i = 0 ; i<SensorCount; i++){
+      Serial.println(get_value(pins[i]));
+    }
+}
+
+void mode_2(){
+  String str = "{";
+      for (int i = 0; i < SensorCount; i++){
+        str += i == 0 ? String(" ") : String(", ");
+        str += "\"" + sensor_id[i] + "\":" + String(get_value(pins[i]));
+      } 
+      Serial.println(str+" }");
+  }
+
+void mode_3(){
+  String str = "[";
+      for (int i = 0; i < SensorCount; i++){
+        str += i == 0 ? "" : ", ";
+        str += String(get_value(pins[i]));
+      } 
+      Serial.println(str+"]");
+  }
+
+void setup()
 {
+  Serial.begin(9600); // 9600 bps
+}
 
-//int fsr_value1 = analogRead(fsr_pin); // 讀取FSR
-//int fsr_value2 = analogRead(fsr_pin2); // 讀取FSR
-//int fsr_value3 = analogRead(fsr_pin3);
-//int fsr_value4 = analogRead(fsr_pin4);
-//int fsr_value5 = analogRead(fsr_pin5);
-//int fsr_value6 = analogRead(fsr_pin6);
-//int fsr_value7 = analogRead(fsr_pin7);
-//int fsr_value8 = analogRead(fsr_pin8);
-//int fsr_value9 = analogRead(fsr_pin9);
-//int fsr_value10 = analogRead(fsr_pin10);
-//int fsr_value11 = analogRead(fsr_pin11);
-//int fsr_value12 = analogRead(fsr_pin12);
-//int fsr_value13 = analogRead(fsr_pin13);
-//int fsr_value14 = analogRead(fsr_pin14);
-//int fsr_value15 = analogRead(fsr_pin15);
-//int fsr_value16 = analogRead(fsr_pin16);
-//Serial.print("The 1 sensor value is : ");
-//Serial.println(fsr_value1);
-//Serial.print("The 2 sensor value is : ");
-//Serial.println(fsr_value2);
-//Serial.print("The 3 sensor value is : ");
-//Serial.println(fsr_value3);
-//Serial.print("The 4 sensor value is : ");
-//Serial.println(fsr_value4);
-//Serial.print("The 5 sensor value is : ");
-//Serial.println(fsr_value5);
-//Serial.print("The 6 sensor value is : ");
-//Serial.println(fsr_value6);
-//Serial.print("The 7 sensor value is : ");
-//Serial.println(fsr_value7);
-//Serial.print("The 8 sensor value is : ");
-//Serial.println(fsr_value8);
-//Serial.print("The 9 sensor value is : ");
-//Serial.println(fsr_value9);
-//Serial.print("The 10 sensor value is : ");
-//Serial.println(fsr_value10);
-//Serial.print("The 11 sensor value is : ");
-//Serial.println(fsr_value11);
-//Serial.print("The 12 sensor value is : ");
-//Serial.println(fsr_value12);
-//Serial.print("The 13 sensor value is : ");
-//Serial.println(fsr_value13);
-//Serial.print("The 14 sensor value is : ");
-//Serial.println(fsr_value14);
-//Serial.print("The 15 sensor value is : ");
-//Serial.println(fsr_value15);
-//Serial.print("The 16 sensor value is : ");
-//Serial.println(fsr_value16);
-//Serial.println("----------------------------");
-//delay(500);
 
-//Connect to python
- if (Serial.available())
- {
-  if ('s'==Serial.read())
+void loop()
+{
+  if (Serial.available())
   {
-    int fsr_value = analogRead(fsr_pin); // 讀取FSR
-    int fsr_value2 = analogRead(fsr_pin2);
-    int fsr_value3 = analogRead(fsr_pin3);
-    int fsr_value4 = analogRead(fsr_pin4);
-    int fsr_value5 = analogRead(fsr_pin5);
-    int fsr_value6 = analogRead(fsr_pin6);
-    int fsr_value7 = analogRead(fsr_pin7);
-    int fsr_value8 = analogRead(fsr_pin8);
-    int fsr_value9 = analogRead(fsr_pin9);
-    int fsr_value10 = analogRead(fsr_pin10);
-    int fsr_value11 = analogRead(fsr_pin11);
-    int fsr_value12 = analogRead(fsr_pin12);
-    int fsr_value13 = analogRead(fsr_pin13);
-    int fsr_value14 = analogRead(fsr_pin14);
-    int fsr_value15 = analogRead(fsr_pin15);
-    int fsr_value16 = analogRead(fsr_pin16);
-    if (fsr_value <200)
-      {
-       Serial.println(0);
+    char s_in = Serial.read();
+    if ('s' == s_in)
+    {
+      if (mode == 1) {
+        mode_1();
       }
-     else 
-      {
-      Serial.println(fsr_value);
+      else if (mode == 2) {
+        mode_2();
       }
-      
-      if (fsr_value2 <200)
-      {
-       Serial.println(0);
+      else if (mode == 3) {
+        mode_3();
       }
-     else 
-      {
-      Serial.println(fsr_value2);
-      }
-
-      if (fsr_value3 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value3);
-      }
-
-      if (fsr_value4 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value4);
-      }
-
-      if (fsr_value5 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value5);
-      }
-
-      if (fsr_value6 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value6);
-      }
-
-      if (fsr_value7 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value7);
-      }
-
-      if (fsr_value8 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value8);
-      }
-
-      if (fsr_value9 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value9);
-      }
-
-      if (fsr_value10 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value10);
-      }
-
-      if (fsr_value11 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value11);
-      }
-
-      if (fsr_value12 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value12);
-      }
-
-      if (fsr_value13 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value13);
-      }
-
-      if (fsr_value14 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value14);
-      }
-
-      if (fsr_value15 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value15);
-      }
-
-      if (fsr_value16 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value16);
-      }
-   
-    delay(300);
+    }
+    else if ('w' == s_in) {
+      Serial.println(BoardName);
+    }
+    else if ('1' <= s_in <= '3') {
+      mode = int(s_in);
+      Serial.println("Set board mode="+s_in);
+    }
   }
-  else
-  {
-    Serial.print("None");
-    delay(300);
-  }
- }
 }
--- arduino/board_2/board_2.ino
+++ arduino/board_2/board_2.ino
@@ -1,157 +1,81 @@
 #include <stdlib.h>
-#define fsr_pin A0
-#define fsr_pin2 A1
-#define fsr_pin3 A2
-#define fsr_pin4 A3
-#define fsr_pin5 A4
-#define fsr_pin6 A5
-#define fsr_pin7 A6
-#define fsr_pin8 A7
-#define fsr_pin9 A8
 
-char serial;
+#define PressureThres 200
+#define SensorCount 9
+#define BoardName "B2"
 
-void setup() 
-{ 
-Serial.begin(9600); // 9600 bps
-} 
-void loop() 
+byte pins[SensorCount] = {A0, A1, A2, A3, A4, A5, A6, A7, A8};
+String sensor_id[SensorCount] = {"4", "9", "14", "19", "20", "21", "22", "23", "24"};
+
+int mode = 1;
+
+int get_value(byte pin){
+  int value = analogRead(pin);
+  return value > PressureThres ? value : 0;
+  }
+
+void mode_1(){
+  for (int i = 0 ; i<SensorCount; i++){
+      Serial.println(get_value(pins[i]));
+    }
+}
+
+void mode_2(){
+  String str = "{";
+      for (int i = 0; i < SensorCount; i++){
+        str += i == 0 ? String(" ") : String(", ");
+        str += "\"" + sensor_id[i] + "\":" + String(get_value(pins[i]));
+      } 
+      Serial.println(str+" }");
+  }
+
+void mode_3(){
+  String str = "[";
+      for (int i = 0; i < SensorCount; i++){
+        str += i == 0 ? "" : ", ";
+        str += String(get_value(pins[i]));
+      } 
+      Serial.println(str+"]");
+  }
+
+void setup()
 {
+  Serial.begin(9600); // 9600 bps
+}
 
-//int fsr_value1 = analogRead(fsr_pin); // 讀取FSR
-//int fsr_value2 = analogRead(fsr_pin2); // 讀取FSR
-//int fsr_value3 = analogRead(fsr_pin3);
-//int fsr_value4 = analogRead(fsr_pin4);
-//int fsr_value5 = analogRead(fsr_pin5);
-//int fsr_value6 = analogRead(fsr_pin6);
-//int fsr_value7 = analogRead(fsr_pin7);
-//int fsr_value8 = analogRead(fsr_pin8);
-//int fsr_value9 = analogRead(fsr_pin9);
 
-//Serial.print("The 1 sensor value is : ");
-//Serial.println(fsr_value1);
-//Serial.print("The 2 sensor value is : ");
-//Serial.println(fsr_value2);
-//Serial.print("The 3 sensor value is : ");
-//Serial.println(fsr_value3);
-//Serial.print("The 4 sensor value is : ");
-//Serial.println(fsr_value4);
-//Serial.print("The 5 sensor value is : ");
-//Serial.println(fsr_value5);
-//Serial.print("The 6 sensor value is : ");
-//Serial.println(fsr_value6);
-//Serial.print("The 7 sensor value is : ");
-//Serial.println(fsr_value7);
-//Serial.print("The 8 sensor value is : ");
-//Serial.println(fsr_value8);
-//Serial.print("The 9 sensor value is : ");
-//Serial.println(fsr_value9);
-
-//Serial.println("----------------------------");
-//delay(500);
-
-//Connect to python
- if (Serial.available())
- {
-  if ('h'==Serial.read())
+void loop()
+{
+  if (Serial.available())
   {
-    int fsr_value = analogRead(fsr_pin); // 讀取FSR
-    int fsr_value2 = analogRead(fsr_pin2); // 讀取FSR
-    int fsr_value3 = analogRead(fsr_pin3);
-    int fsr_value4 = analogRead(fsr_pin4);
-    int fsr_value5 = analogRead(fsr_pin5);
-    int fsr_value6 = analogRead(fsr_pin6);
-    int fsr_value7 = analogRead(fsr_pin7);
-    int fsr_value8 = analogRead(fsr_pin8);
-    int fsr_value9 = analogRead(fsr_pin9);
-
-       if (fsr_value <200)
+    char s_in = Serial.read();
+    switch (s_in)
+    {
+    case 's':
+      switch (mode)
       {
-       Serial.println(0);
+      case 1:
+        mode_1();
+        break;
+      case 2:
+        mode_2();
+        break;
+      case 3:
+        mode_3();
+        break;
+      default:
+        break;
       }
-     else 
-      {
-      Serial.println(fsr_value);
-      }
-      
-      if (fsr_value2 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value2);
-      }
-
-      if (fsr_value3 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value3);
-      }
-
-      if (fsr_value4 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value4);
-      }
-
-      if (fsr_value5 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value5);
-      }
-
-      if (fsr_value6 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value6);
-      }
-
-      if (fsr_value7 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value7);
-      }
-
-      if (fsr_value8 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value8);
-      }
-
-      if (fsr_value9 <200)
-      {
-       Serial.println(0);
-      }
-     else 
-      {
-      Serial.println(fsr_value9);
-      }
-    
-    delay(300);
+      break;
+    case 'w':
+      Serial.println(BoardName);
+      break;
+    case '1': case '2': case '3':
+      mode = int(s_in);
+      Serial.println("Set board mode="+s_in);
+      break;
+    default:
+      break;
+    }
   }
-  else
-  {
-    Serial.print("None");
-    delay(300);
-  }
- }
 }
