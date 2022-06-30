da6555a303b7a478d42a20d5d6b4ac0d7a3de35b
aclich <aclich.32@gmail.com>
2022-06-30 22:02:11 +0800

    remove unused script

--- new_main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from GUI.mainapp import MainApp
-from GUI.login_page import LoginPage
-
-
-if __name__ == "__main__":
-    mainapp = MainApp()
-    login_page = LoginPage(mainapp)
-    mainapp.mainloop()
